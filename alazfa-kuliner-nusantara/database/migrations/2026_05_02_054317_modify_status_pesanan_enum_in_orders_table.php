8835a4abe70401446cddb87b296e2e0e3c9ac6ca
Azmi Sophia Wakova <azmisophiawakova@gmail.com>
2026-06-14 16:10:44 +0700

    add: New migrations penambahan provinsi

---
+++ alazfa-kuliner-nusantara/database/migrations/2026_05_02_054317_modify_status_pesanan_enum_in_orders_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::statement("ALTER TABLE orders MODIFY COLUMN status_pesanan VARCHAR(50) DEFAULT 'menunggu'");
+    }
+
+    public function down(): void
+    {
+        DB::statement("ALTER TABLE orders MODIFY COLUMN status_pesanan ENUM('konfirmasi pesanan', 'pesanan diproses', 'pesanan dikirim', 'pesanan selesai', 'pesanan dibatalkan') DEFAULT 'konfirmasi pesanan'");
+    }
+};
